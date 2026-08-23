afedb7c850205bf5cfb11038b2e98ef8a7d84cd4
Towhidul Islam <towhidul.islam5797@gmail.com>
2026-08-23 17:17:24 +0600

    HUD System Added

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: PlaytimeRhyme
 - Unity version: Unity 6000.3.10f1
 - Active game object:
-  - Name: TileTray
+  - Name: Button_Hint
   - Tag: Untagged
   - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/_Scripts/_Core/GameSession.cs
+++ Assets/_Scripts/_Core/GameSession.cs
@@ -48,8 +48,388 @@
 #endregion
 
 #region Phase 1 Sprint 5 - GameSession (puzzle display and gameplay)
+//using UnityEngine;
+//using UnityEngine.UI;
+//using System.Collections.Generic;
+
+//public class GameSession : MonoBehaviour
+//{
+//    #region Configuration
+//    [SerializeField] PuzzleLoader loader;
+//    [SerializeField] Image puzzleImageDisplay;
+//    [SerializeField] Transform tileTray;
+//    [SerializeField] Transform answerSlotRow;
+//    [SerializeField] GameObject tilePrefab;
+//    [SerializeField] GameObject slotPrefab;
+//    #endregion
+
+//    #region State
+//    int currentIndex = 0;
+//    List<AnswerSlot> currentSlots = new List<AnswerSlot>();
+//    #endregion
+
+//    #region Unity Lifecycle
+//    void Start()
+//    {
+//        LoadPuzzle();
+//    }
+//    #endregion
+
+//    #region Puzzle Navigation
+//    public PuzzleData GetCurrentPuzzle()
+//    {
+//        return loader.puzzles[currentIndex];
+//    }
+
+//    public bool HasNextPuzzle()
+//    {
+//        return currentIndex + 1 < loader.puzzles.Length;
+//    }
+
+//    public void LoadNextPuzzle()
+//    {
+//        if (HasNextPuzzle())
+//        {
+//            currentIndex++;
+//            LoadPuzzle();
+//        }
+//    }
+//    #endregion
+
+//    #region Puzzle Setup
+//    void LoadPuzzle()
+//    {
+//        ClearTray();
+//        ClearSlots();
+
+//        PuzzleData puzzle = GetCurrentPuzzle();
+
+//        puzzleImageDisplay.sprite = loader.GetSpriteForPuzzle(puzzle);
+
+//        SpawnSlots(puzzle.answerWords);
+//        SpawnTiles(puzzle.jumbleLetters);
+//    }
+
+//    void ClearTray()
+//    {
+//        foreach (Transform child in tileTray)
+//        {
+//            Destroy(child.gameObject);
+//        }
+//    }
+
+//    void ClearSlots()
+//    {
+//        foreach (Transform child in answerSlotRow)
+//        {
+//            Destroy(child.gameObject);
+//        }
+//        currentSlots.Clear();
+//    }
+
+//    void SpawnSlots(string[] answerWords)
+//    {
+//        for (int w = 0; w < answerWords.Length; w++)
+//        {
+//            string word = answerWords[w];
+
+//            for (int i = 0; i < word.Length; i++)
+//            {
+//                GameObject slotObject = Instantiate(slotPrefab, answerSlotRow);
+//                AnswerSlot slot = slotObject.GetComponent<AnswerSlot>();
+//                currentSlots.Add(slot);
+//            }
+
+//            bool isLastWord = (w == answerWords.Length - 1);
+//            if (!isLastWord)
+//            {
+//                SpawnWordGap();
+//            }
+//        }
+//    }
+
+//    void SpawnWordGap()
+//    {
+//        GameObject gap = new GameObject("WordGap", typeof(RectTransform));
+//        gap.transform.SetParent(answerSlotRow, false);
+//        LayoutElement layoutElement = gap.AddComponent<LayoutElement>();
+//        layoutElement.minWidth = 40f;
+//    }
+
+//    void SpawnTiles(string jumbleLetters)
+//    {
+//        foreach (char letter in jumbleLetters)
+//        {
+//            GameObject tileObject = Instantiate(tilePrefab, tileTray);
+//            PuzzleTile tile = tileObject.GetComponent<PuzzleTile>();
+//            tile.Setup(letter, this);
+//        }
+//    }
+//    #endregion
+
+//    #region Tile Placement
+//    public void OnTileTapped(PuzzleTile tile)
+//    {
+//        AnswerSlot nextEmptySlot = FindNextEmptySlot();
+//        if (nextEmptySlot == null) return;
+
+//        nextEmptySlot.PlaceLetter(tile.Letter);
+//        Destroy(tile.gameObject);
+
+//        if (AllSlotsFilled())
+//        {
+//            CheckAnswer();
+//        }
+//    }
+
+//    AnswerSlot FindNextEmptySlot()
+//    {
+//        foreach (AnswerSlot slot in currentSlots)
+//        {
+//            if (!slot.IsFilled)
+//            {
+//                return slot;
+//            }
+//        }
+//        return null;
+//    }
+
+//    bool AllSlotsFilled()
+//    {
+//        foreach (AnswerSlot slot in currentSlots)
+//        {
+//            if (!slot.IsFilled)
+//            {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//    #endregion
+
+//    #region Answer Checking
+//    void CheckAnswer()
+//    {
+//        string correctAnswer = string.Join("", GetCurrentPuzzle().answerWords);
+
+//        for (int i = 0; i < currentSlots.Count; i++)
+//        {
+//            char correctLetter = correctAnswer[i];
+//            char placedLetter = currentSlots[i].GetLetter();
+
+//            if (placedLetter == correctLetter)
+//            {
+//                currentSlots[i].ShowCorrect();
+//            }
+//            else
+//            {
+//                currentSlots[i].ShowWrong();
+//            }
+//        }
+//    }
+//    #endregion
+//}
+#endregion
+
+#region Phase 1 Sprint 6 - GameSession (level number display)
+//using UnityEngine;
+//using UnityEngine.UI;
+//using TMPro;
+//using System.Collections.Generic;
+
+//public class GameSession : MonoBehaviour
+//{
+//    #region Configuration
+//    [SerializeField] PuzzleLoader loader;
+//    [SerializeField] Image puzzleImageDisplay;
+//    [SerializeField] Transform tileTray;
+//    [SerializeField] Transform answerSlotRow;
+//    [SerializeField] GameObject tilePrefab;
+//    [SerializeField] GameObject slotPrefab;
+//    [SerializeField] TMP_Text levelText;
+//    #endregion
+
+//    #region State
+//    int currentIndex = 0;
+//    List<AnswerSlot> currentSlots = new List<AnswerSlot>();
+//    #endregion
+
+//    #region Unity Lifecycle
+//    void Start()
+//    {
+//        LoadPuzzle();
+//    }
+//    #endregion
+
+//    #region Puzzle Navigation
+//    public PuzzleData GetCurrentPuzzle()
+//    {
+//        return loader.puzzles[currentIndex];
+//    }
+
+//    public bool HasNextPuzzle()
+//    {
+//        return currentIndex + 1 < loader.puzzles.Length;
+//    }
+
+//    public void LoadNextPuzzle()
+//    {
+//        if (HasNextPuzzle())
+//        {
+//            currentIndex++;
+//            LoadPuzzle();
+//        }
+//    }
+//    #endregion
+
+//    #region Puzzle Setup
+//    void LoadPuzzle()
+//    {
+//        ClearTray();
+//        ClearSlots();
+
+//        PuzzleData puzzle = GetCurrentPuzzle();
+
+//        puzzleImageDisplay.sprite = loader.GetSpriteForPuzzle(puzzle);
+//        UpdateLevelText(puzzle);
+
+//        SpawnSlots(puzzle.answerWords);
+//        SpawnTiles(puzzle.jumbleLetters);
+//    }
+
+//    void UpdateLevelText(PuzzleData puzzle)
+//    {
+//        if (levelText != null)
+//        {
+//            levelText.text = $"Level {puzzle.levelNumber}";
+//        }
+//    }
+
+//    void ClearTray()
+//    {
+//        foreach (Transform child in tileTray)
+//        {
+//            Destroy(child.gameObject);
+//        }
+//    }
+
+//    void ClearSlots()
+//    {
+//        foreach (Transform child in answerSlotRow)
+//        {
+//            Destroy(child.gameObject);
+//        }
+//        currentSlots.Clear();
+//    }
+
+//    void SpawnSlots(string[] answerWords)
+//    {
+//        for (int w = 0; w < answerWords.Length; w++)
+//        {
+//            string word = answerWords[w];
+
+//            for (int i = 0; i < word.Length; i++)
+//            {
+//                GameObject slotObject = Instantiate(slotPrefab, answerSlotRow);
+//                AnswerSlot slot = slotObject.GetComponent<AnswerSlot>();
+//                currentSlots.Add(slot);
+//            }
+
+//            bool isLastWord = (w == answerWords.Length - 1);
+//            if (!isLastWord)
+//            {
+//                SpawnWordGap();
+//            }
+//        }
+//    }
+
+//    void SpawnWordGap()
+//    {
+//        GameObject gap = new GameObject("WordGap", typeof(RectTransform));
+//        gap.transform.SetParent(answerSlotRow, false);
+//        LayoutElement layoutElement = gap.AddComponent<LayoutElement>();
+//        layoutElement.minWidth = 40f;
+//    }
+
+//    void SpawnTiles(string jumbleLetters)
+//    {
+//        foreach (char letter in jumbleLetters)
+//        {
+//            GameObject tileObject = Instantiate(tilePrefab, tileTray);
+//            PuzzleTile tile = tileObject.GetComponent<PuzzleTile>();
+//            tile.Setup(letter, this);
+//        }
+//    }
+//    #endregion
+
+//    #region Tile Placement
+//    public void OnTileTapped(PuzzleTile tile)
+//    {
+//        AnswerSlot nextEmptySlot = FindNextEmptySlot();
+//        if (nextEmptySlot == null) return;
+
+//        nextEmptySlot.PlaceLetter(tile.Letter);
+//        Destroy(tile.gameObject);
+
+//        if (AllSlotsFilled())
+//        {
+//            CheckAnswer();
+//        }
+//    }
+
+//    AnswerSlot FindNextEmptySlot()
+//    {
+//        foreach (AnswerSlot slot in currentSlots)
+//        {
+//            if (!slot.IsFilled)
+//            {
+//                return slot;
+//            }
+//        }
+//        return null;
+//    }
+
+//    bool AllSlotsFilled()
+//    {
+//        foreach (AnswerSlot slot in currentSlots)
+//        {
+//            if (!slot.IsFilled)
+//            {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//    #endregion
+
+//    #region Answer Checking
+//    void CheckAnswer()
+//    {
+//        string correctAnswer = string.Join("", GetCurrentPuzzle().answerWords);
+
+//        for (int i = 0; i < currentSlots.Count; i++)
+//        {
+//            char correctLetter = correctAnswer[i];
+//            char placedLetter = currentSlots[i].GetLetter();
+
+//            if (placedLetter == correctLetter)
+//            {
+//                currentSlots[i].ShowCorrect();
+//            }
+//            else
+//            {
+//                currentSlots[i].ShowWrong();
+//            }
+//        }
+//    }
+//    #endregion
+//}
+#endregion
+
+#region Phase 1 Sprint 7 - GameSession (level text + Hint/Clear/Undo/Scramble support)
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using System.Collections.Generic;
 
 public class GameSession : MonoBehaviour
@@ -61,11 +441,14 @@ public class GameSession : MonoBehaviour
     [SerializeField] Transform answerSlotRow;
     [SerializeField] GameObject tilePrefab;
     [SerializeField] GameObject slotPrefab;
+    [SerializeField] TMP_Text levelText;
     #endregion
 
     #region State
     int currentIndex = 0;
     List<AnswerSlot> currentSlots = new List<AnswerSlot>();
+    List<PuzzleTile> currentTiles = new List<PuzzleTile>();
+    Stack<(AnswerSlot slot, char letter)> placementHistory = new Stack<(AnswerSlot, char)>();
     #endregion
 
     #region Unity Lifecycle
@@ -101,13 +484,23 @@ public class GameSession : MonoBehaviour
     {
         ClearTray();
         ClearSlots();
+        placementHistory.Clear();
 
         PuzzleData puzzle = GetCurrentPuzzle();
 
         puzzleImageDisplay.sprite = loader.GetSpriteForPuzzle(puzzle);
+        UpdateLevelText(puzzle);
 
         SpawnSlots(puzzle.answerWords);
         SpawnTiles(puzzle.jumbleLetters);
+    }
+
+    void UpdateLevelText(PuzzleData puzzle)
+    {
+        if (levelText != null)
+        {
+            levelText.text = $"Level {puzzle.levelNumber}";
+        }
     }
 
     void ClearTray()
@@ -116,6 +509,7 @@ public class GameSession : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
+        currentTiles.Clear();
     }
 
     void ClearSlots()
@@ -160,20 +554,37 @@ public class GameSession : MonoBehaviour
     {
         foreach (char letter in jumbleLetters)
         {
-            GameObject tileObject = Instantiate(tilePrefab, tileTray);
-            PuzzleTile tile = tileObject.GetComponent<PuzzleTile>();
-            tile.Setup(letter, this);
+            SpawnSingleTile(letter);
         }
+    }
+
+    PuzzleTile SpawnSingleTile(char letter)
+    {
+        GameObject tileObject = Instantiate(tilePrefab, tileTray);
+        PuzzleTile tile = tileObject.GetComponent<PuzzleTile>();
+        tile.Setup(letter, this);
+        currentTiles.Add(tile);
+        return tile;
     }
     #endregion
 
     #region Tile Placement
     public void OnTileTapped(PuzzleTile tile)
     {
+        PlaceTile(tile);
+    }
+
+    void PlaceTile(PuzzleTile tile)
+    {
         AnswerSlot nextEmptySlot = FindNextEmptySlot();
         if (nextEmptySlot == null) return;
 
-        nextEmptySlot.PlaceLetter(tile.Letter);
+        char letter = tile.Letter;
+
+        nextEmptySlot.PlaceLetter(letter);
+        placementHistory.Push((nextEmptySlot, letter));
+
+        currentTiles.Remove(tile);
         Destroy(tile.gameObject);
 
         if (AllSlotsFilled())
@@ -228,6 +639,53 @@ public class GameSession : MonoBehaviour
         }
     }
     #endregion
+
+    #region HUD Actions
+    public void ClearAnswer()
+    {
+        LoadPuzzle();
+    }
+
+    public void UndoLastPlacement()
+    {
+        if (placementHistory.Count == 0) return;
+
+        var (slot, letter) = placementHistory.Pop();
+        slot.ShowEmpty();
+        SpawnSingleTile(letter);
+    }
+
+    public void GiveHint()
+    {
+        AnswerSlot nextEmptySlot = FindNextEmptySlot();
+        if (nextEmptySlot == null) return;
+
+        string correctAnswer = string.Join("", GetCurrentPuzzle().answerWords);
+        int slotIndex = currentSlots.IndexOf(nextEmptySlot);
+        char neededLetter = correctAnswer[slotIndex];
+
+        PuzzleTile matchingTile = currentTiles.Find(t => t.Letter == neededLetter);
+        if (matchingTile != null)
+        {
+            PlaceTile(matchingTile);
+        }
+    }
+
+    public void ScrambleRemaining()
+    {
+        List<PuzzleTile> shuffled = new List<PuzzleTile>(currentTiles);
+
+        for (int i = shuffled.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            (shuffled[i], shuffled[randomIndex]) = (shuffled[randomIndex], shuffled[i]);
+        }
+
+        for (int i = 0; i < shuffled.Count; i++)
+        {
+            shuffled[i].transform.SetSiblingIndex(i);
+        }
+    }
+    #endregion
 }
 #endregion
-
